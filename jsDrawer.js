b148d07299d60bc91c1633f89a12837ac0957421
Jakub <99950753+Dantevincivezz@users.noreply.github.com>
2022-06-25 03:12:32 +0100

    update

--- jsDrawer.js
+++ jsDrawer.js
@@ -1,10 +1,39 @@
+var colorWell;
+var defaultColor = "#000000";
+var currentColor = "#000000";
+
+window.addEventListener("load", startup, false);
+
+function startup() {
+    colorWell = document.querySelector("#colorPicker");
+    colorWell.value = defaultColor;
+    colorWell.addEventListener("change", updateAll, false);
+    colorWell.select();
+
+}
+function updateAll(event) {
+    currentColor = event.target.value;
+};
+
+
 function changeColor(i){
 
-    if(i.style.backgroundColor !== "black")
+    if(i.style.backgroundColor !== currentColor)
         {
-        i.style.backgroundColor = "black";
+        i.style.backgroundColor = currentColor;
         console.log(i);
-        }else{i.style.backgroundColor = "azure"}
+        }
+        else{ i.style.backgroundColor = "white"}
     
     }
+
+    function clearAll(){
+        location.reload();
+        console.log('done');
+    }
+
+    function useWhite(event){
+        event.target.value = '#ffffff';
+    }
+
     
--- main.js
+++ main.js
@@ -1,40 +1,27 @@
 $(document).ready(function(){
 
-    // const canvas = $('#canvas');
-    var devWidth = window.innerWidth; // x = vw
-    var devHeight = window.innerHeight; // y = vh
+    var devWidth = Math.round(window.innerWidth/10);
+    let devHeight = Math.round(window.innerHeight/10);
 
-    let _devWidth = Math.round(devWidth/10);  // a = round(100vw/10)
-    let _devHeight = Math.round(devHeight/10); // b = round(100vh/10)
-
-    let _cellMax = ((devWidth * devHeight)/100)+20;
+    let _cellMax = ((devWidth * devHeight))+20;
 
 $('#canv').css({
     "display":"grid",
     "margin":"auto",
     "justify-content":"center",
-    "grid-template-columns":`repeat(${_devWidth},1fr)`,
-    "grid-template-rows":`repeat(${_devHeight},1fr)`
+    "grid-template-columns":`repeat(${devWidth},1fr)`,
+    "grid-template-rows":`repeat(${devHeight},1fr)`
     })
-// "+_devWidth+"
-// "+_devHeight+"
 
 for (let i = 0; i <= _cellMax; i++) {
 
-    $("<div id='cell' class='"+i+"' onclick='changeColor(this);'> </div>").appendTo("#canv")
+    $("<div id='cell' class='"+i+"' onclick='changeColor(this)' style='background-color: rgb(255, 255, 255);'> </div>").appendTo("#canv")
 
 }
 
 console.log("devWidth : "+ devWidth)
 console.log("devHeight : "+ devHeight)
-console.log("_devWidth : "+ _devWidth)
-console.log("_devHeight : "+ _devHeight)
 console.log("_cellMax : "+ _cellMax)
-
-// ${}
-
-
-
 
 })
 
